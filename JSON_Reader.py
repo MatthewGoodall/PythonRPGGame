d646c35d032ea7e01fdc5dd01dcf5c3862a0e9f8
MatthewGoodall <matthewgoodall1579@gmail.com>
2017-03-05 21:12:57 -0800

    NPC File now as mother and bad guy in it

--- JSON_Reader.py
+++ JSON_Reader.py
@@ -1,7 +1,7 @@
 import json
 
 
-def JSON_Reader(json_data):
+def JSON_Reader(json_data, data_entry, name):
     with open(json_data) as json_file:
         json_read = json.load(json_file)
-        print(json_read['b']['Hello'])
+        print(json_read["NPC"][data_entry][name])
