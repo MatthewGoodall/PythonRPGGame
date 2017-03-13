4fab7fe3c3f110734b2b06eb266186cc8a122151
Cyborger <joe.zlonicky@gmail.com>
2017-03-13 13:38:01 -0700

    Added stuff to inventory class

--- Inventory.py
+++ Inventory.py
@@ -1,3 +1,28 @@
 class Inventory:
     def __init__(self):
         self.items = []
+        self.max_items = 30
+        self.gold = 0
+
+    def AddItem(self, item):
+        if len(self.items) >= 30:
+            print("Can not add item, full inventory")
+        else:
+            self.items.append(item)
+
+    def AddGold(self, amount):
+        self.gold += amount
+
+    def RemoveItem(self, item):
+        if item in self.items:
+            self.items.remove(item)
+
+    def CanPayGold(self, amount):
+        if self.gold - amount < 0:
+            print("can not afford")
+            return False
+        else:
+            return True
+
+    def DecreaseGold(self, amount):
+        self.gold -= amount
