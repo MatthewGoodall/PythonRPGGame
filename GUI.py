448064d632a4df1a6f375be52d79500e57228eef
Cyborger <joe.zlonicky@gmail.com>
2017-03-14 20:30:37 -0700

    Centered pause screen

--- GUI.py
+++ GUI.py
@@ -10,7 +10,9 @@ class GUI:
         self.gui_items.extend((self.health_bar, self.mana_bar))
 
         self.pause_screen_image = pygame.image.load("Resources/SinglePhotos/PauseMenu.png")
-        self.pause_screen = GUI_Item(self.pause_screen_image, 450, 300)
+        self.pause_screen = GUI_Item(self.pause_screen_image, 0, 0)
+        self.pause_screen.rect.x = 1024/2 - self.pause_screen.rect.width/2
+        self.pause_screen.rect.y = 576/2 - self.pause_screen.rect.height/2
 
     def Update(self, player):
         self.health_bar.Update(player.current_health, player.maximum_health)
